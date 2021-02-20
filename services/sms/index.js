88036dcba6cd402a18658187a486f6a08bfe544a
ibednorz <bednorz.ireneusz@gmail.com>
2021-02-20 19:08:13 +0100

    add sms service

---
+++ services/sms/index.js
@@ -0,0 +1,12 @@
+const Nexmo = require('nexmo')
+
+const nexmo = new Nexmo({
+    apiKey: 'e5c1a352',
+    apiSecret: 'nD9CM2Hf3qjvzVZE',
+  });
+
+const sendSms = (from, number, text) => {
+    nexmo.message.sendSms(from, number, text)
+}
+
+//number format - 48111222333
