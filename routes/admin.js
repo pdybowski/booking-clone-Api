817e8b74651b2f77772302fe2b73ec66bcf03e67
ibednorz <bednorz.ireneusz@gmail.com>
2021-02-26 19:21:10 +0100

    change delete router

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,12 @@ const express = require('express')
 const User = require('../models/user')
 const ApiError = require('../helpers/apiError')
 const router = express.Router()
-const { isAdmin } = require('../middleware/role')
 const { HOTEL_OWNER_ROLE, USER_ROLE } = require('../models/roles')
 
 router.get('/', async (req, res) => {
   const users = await User.find()
 
-  res.json(users)
+  res.send(users)
 })
 
 router.get('/owners', async (req, res) => {
@@ -22,10 +21,7 @@ router.put('/owner/accept/:email', async (req, res) => {
   const email = req.params.email
 
   try {
-    const owner = await User.updateOne(
-      { email: email },
-      { role: HOTEL_OWNER_ROLE }
-    )
+    await User.updateOne({ email: email }, { role: HOTEL_OWNER_ROLE })
 
     res.status(200).json('Done')
   } catch (err) {
@@ -37,15 +33,15 @@ router.delete('/owner/:email', async (req, res) => {
   const email = req.params.email
 
   try {
-    const id = await User.findOneAndDelete({
+    const user = await User.findOneAndDelete({
       email: email,
       role: HOTEL_OWNER_ROLE,
     })
-    if (!id) {
+    if (!user) {
       throw new ApiError(400, 'Wrong email or user is not a hotel owner')
     }
 
-    res.status(200).json(id)
+    res.status(200).json(user)
   } catch (err) {
     throw new ApiError(500, 'Something went wrong')
   }
@@ -55,15 +51,15 @@ router.delete('/:email', async (req, res) => {
   const email = req.params.email
 
   try {
-    const id = await User.findOneAndDelete({
+    const user = await User.findOneAndDelete({
       email: email,
       role: USER_ROLE,
     })
-    if (!id) {
+    if (!user) {
       throw new ApiError(400, 'Wrong email')
     }
 
-    res.status(200).json(id)
+    res.status(200).json(user)
   } catch (err) {
     throw new ApiError(500, 'Something went wrong')
   }
