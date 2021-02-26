07770884670e114013c263d56faad281ce18da4c
ibednorz <bednorz.ireneusz@gmail.com>
2021-02-26 10:03:26 +0100

    add config function

--- services/email/index.js
+++ services/email/index.js
@@ -2,27 +2,30 @@ const nodemailer = require('nodemailer')
 const handlebars = require('handlebars')
 const fs = require('fs')
 const path = require('path')
-const { emailUser, emailPass } = require('../../config/local.json')
+const config = require('config')
 
-function emailSend(email, subject, view, username, hotel) {
+const mailUser = config.get('mail.user')
+const mailPass = config.get('mail.pass')
+
+function mailSend(mail, subject, view, username, hotel) {
   const filePath = path.join(__dirname, `./views/${view}.html`)
   const source = fs.readFileSync(filePath, 'utf-8').toString()
   const template = handlebars.compile(source)
   const replacements = {
     username: username,
-    hotel: hotel
+    hotel: hotel,
   }
   const htmlToSend = template(replacements)
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: emailUser,
-      pass: emailPass,
+      user: mailUser,
+      pass: mailPass,
     },
   })
   const mailOptions = {
     from: '"Booking Clone API" <bookingcloneapi@gmail.com>',
-    to: email,
+    to: mail,
     subject: subject,
     html: htmlToSend,
   }
--- services/sms/index.js
+++ services/sms/index.js
@@ -1,14 +1,16 @@
 const Nexmo = require('nexmo')
-const { smsAPI, smsPass } = require('../../config/local.json')
+const config = require('config')
 
+const smsAPI = config.get('sms.API')
+const smsPass = config.get('sms.pass')
 
 const nexmo = new Nexmo({
-    apiKey: smsAPI,
-    apiSecret: smsPass,
-  });
+  apiKey: smsAPI,
+  apiSecret: smsPass,
+})
 
 const smsSend = (from, number, text) => {
-    nexmo.message.sendSms(from, number, text)
+  nexmo.message.sendSms(from, number, text)
 }
 
 //number format - 48111222333
