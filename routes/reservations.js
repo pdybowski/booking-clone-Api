42c2c660250a33693e522880af78e47de979214e
hoolek77 <szymon.kin@gmail.com>
2021-03-01 16:54:31 +0100

    removed code that wasn't meant to be there

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const { Reservation, validate } = require('../models/reservation')
-const { Room } = require('../models/room')
 const ApiError = require('../helpers/apiError')
 const router = express.Router()
-
-// TODO: add auth middlewear, when it will be ready
 
 router.get('/', async (req, res) => {
   const reservations = await Reservation.find()
@@ -17,22 +14,9 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body)
   if (error) throw new ApiError(400, error.details[0].message)
 
-  const room = await Room.findById(req.body.roomId)
-  if (!room) throw new ApiError(400, 'Wrong room ID.')
-
-  const { occupiedDates } = room
-  const { startDate, endDate } = req.body
-
   let reservation = new Reservation(req.body)
 
   await reservation.save()
-
-  occupiedDates.push({
-    startDate: new Date(startDate),
-    endDate: new Date(endDate),
-  })
-
-  await room.save()
 
   res.send(reservation)
 })
