66042088204d2d99a7df698a4e32cfc5d87b7691
Piotr <p.dybowski13@gmail.com>
2021-03-02 19:20:48 +0100

    fix

--- controllers/hotelsController.js
+++ controllers/hotelsController.js
@@ -4,7 +4,7 @@ const { getFreeRooms } = require('../services/hotelsService')
 
 exports.getFreeRooms = async (req, res, next) => {
   try {
-    const freeRooms = getFreeRooms(req)
+    const freeRooms = await getFreeRooms(req)
     res.status(200).send(freeRooms)
   } catch (error) {
     next(new ApiError(400, 'Free room data can not be fetched.'))
--- services/hotelsService.js
+++ services/hotelsService.js
@@ -1,6 +1,6 @@
 const ApiError = require('../helpers/apiError')
-const Hotel = require('../models/hotel')
-const Reservation = require('../models/reservation')
+const { Hotel } = require('../models/hotel')
+const { Reservation } = require('../models/reservation')
 
 exports.getFreeRooms = async (req) => {
   if (!req.query.startDate || !req.query.endDate)
@@ -9,25 +9,20 @@ exports.getFreeRooms = async (req) => {
   const { hotelId } = req.params
   const { startDate, endDate } = req.query
 
+  const hotel = await Hotel.findById(hotelId)
+  if(!hotel) throw new ApiError(404, 'Hotel not found')
+
   const freeRooms = []
+  const rooms = hotel.rooms
 
-  const rooms = await Hotel.find({ _id: hotelId }).rooms
-
-  rooms.forEach(async (room) => {
-    const roomReservations = await Reservation.find({ roomId: room._id })
-    if (roomReservations.count() == 0) return freeRooms.push(room)
-
-    const occupiedRR = await roomReservations.find({
-      $or: [
-        {
-          startDate: { $and: [{ $gte: startDate }, { $lte: endDate }] },
-          endDate: { $and: [{ $lte: endDate }, { $gte: startDate }] },
-        },
-      ],
-    })
-
-    if (occupiedRR.count() === 0) return freeRooms.push(room)
-  })
-
+  for(const room of rooms) {
+    const roomReservations = await Reservation.find({ room: room._id })
+    if (roomReservations.length == 0) {
+      freeRooms.push(room)
+    } else {
+      const occupiedRR = roomReservations.some(rr => (rr.startDate.toISOString() <= startDate && rr.endDate.toISOString() > startDate) || (rr.startDate.toISOString() < endDate && rr.endDate.toISOString() >= endDate))
+      if (!occupiedRR) freeRooms.push(room)
+    }
+  }
   return freeRooms
 }
